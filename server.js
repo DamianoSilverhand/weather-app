c31de559df3bc0933d60db5ef7ffeaa3a4a9060c
Damiano Chintala <damianochintala@gmail.com>
2019-12-02 16:31:04 +0200

    resolving errors in post method

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.render('index')
@@ -12,10 +14,8 @@ app.post('/', function (req, res) {
   console.log(req.body.city);
 })
 
-const bodyParser = require('body-parser');
-// ...
-// ...
-app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 app.listen(3002, function () {
   console.log('Example app listening on port 3002!')
