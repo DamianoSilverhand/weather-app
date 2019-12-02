26c154bdc5036e407a9de526fa5252d0622e00b3
Damiano Chintala <damianochintala@gmail.com>
2019-12-02 16:06:21 +0200

    adding body-parser

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.render('index')
 })
+const bodyParser = require('body-parser');
+// ...
+// ...
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3002, function () {
   console.log('Example app listening on port 3002!')
