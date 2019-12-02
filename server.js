9d39db699b4467ec7eeb3a6113d498d90c6090dc
Damiano Chintala <damianochintala@gmail.com>
2019-12-02 16:53:21 +0200

    Changing post method to display data in web app

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const request = require('request');
+const apiKey ='4176b01f3b069bf25ca0ad38079389b6'
 const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -10,8 +12,21 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-  res.render('index');
-  console.log(req.body.city);
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
 
 
